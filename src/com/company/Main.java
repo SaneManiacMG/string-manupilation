8282b76198a187cdc92ee71b8679227513cf8087
Mogomotsi <mmoroane@hotmail.com>
2020-12-04 02:20:18 +0200

    Added more string manupilations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        String myString = "Jack,Jill,Steve,Joll";
+        //String myString = "Jack + Jill + Steve + Joll";
+        String river = "Mississippi";
+        String riverPart = river.substring(4, 8);
 
-        String[] splitString = myString.split(",");
+        String[] splitThis = river.split("s");
+        //String[] splitString = myString.split(" + ");
 
-        System.out.println(Arrays.toString(splitString));
+        System.out.println(Arrays.toString(splitThis));
+        System.out.println(Arrays.toString(new String[]{riverPart}));
     }
 }
