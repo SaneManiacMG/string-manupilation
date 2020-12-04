5f9294e2d8b91b952d2fbe99d186a8f394529b7a
Mogomotsi <mmoroane@hotmail.com>
2020-12-04 11:20:08 +0200

    Revert "Addded regex and matcher, pattern"
    This reverts commit 92205870

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,6 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static sun.misc.Version.println;
 
 public class Main {
 
@@ -12,13 +8,6 @@ public class Main {
         //String myString = "Jack + Jill + Steve + Joll";
         String river = "MississippiMississippiMississippi";
         String riverPart = river.substring(4, 8);
-
-        Pattern p = Pattern.compile("Mi(.*?)pi");
-        Matcher m = p.matcher(river);
-
-        while (m.find()) {
-            System.out.println(m.group(1));
-        }
 
         String[] splitThis = river.split("s");
         //String[] splitString = myString.split(" + ");
